ef8623ce0e8485b2e074bae740ff3ef1c54ca5ca
INEO-ZHANG <1642047843@qq.com>
2022-12-08 19:02:22 +0800

    info of this commit

---
+++ myFirstVue.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+      <h1>{{message}}</h1>
+  
+      <p>
+        this is
+        <a
+          href="https://ineo-zhang.github.io/"
+        >my Blogs</a>.
+      </p>
+    </div>
+  
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+          message: 'My First SFC'
+        };
+      }
+    }
+  </script>
+  
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  
+  a {
+    color: #7a1111;
+  }
+  </style>
